36f13f4339c64f73f07a811bb5e5f39a776b1d2b
zhangshuyang <zhangshuyang@outlook.com>
2022-05-29 02:10:40 +0800

    feat: now channel name can be updated by message and bot

--- app/utils/channel_utils.py
+++ app/utils/channel_utils.py
@@ -1,7 +1,7 @@
-from khl import Message
+from khl import Message, Bot
 
 
-async def update_channel_name(msg: Message, channel_id: str, new_name: str):
+async def update_channel_name_by_message(msg: Message, channel_id: str, new_name: str):
     method = "POST"
     route = "channel/update"
     json = {
@@ -10,3 +10,13 @@ async def update_channel_name(msg: Message, channel_id: str, new_name: str):
     }
 
     await msg.ctx.gate.request(method=method, route=round, json=json)    
+
+async def update_channel_name_by_bot(bot: Bot, channel_id: str, new_name: str):
+    method = "POST"
+    route = "channel/update"
+    json = {
+        "channel_id": channel_id,
+        "name": new_name,
+    }
+
+    await bot.client.gate.request(method=method, route=round, json=json)    
